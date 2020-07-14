4bcb15b59a978e7902d9555d64579fef854d9d6c
UsamaEjaz0 <uejaz0@gmail.com>
2020-07-14 18:06:11 +0500

    Added service provider

--- src/marsons/yard/AddVendorController.java
+++ src/marsons/yard/AddVendorController.java
@@ -5,9 +5,21 @@
  */
 package marsons.yard;
 
+import connection.MyConnection;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -19,9 +31,59 @@ public class AddVendorController implements Initializable {
     /**
      * Initializes the controller class.
      */
+    ObservableList pGroupList = FXCollections.observableArrayList();
+    @FXML
+    private TextField Cname;
+
+    @FXML
+    private TextField pNum;
+
+    @FXML
+    private TextField sAddr;
+
+    @FXML
+    private TextField addr;
+
+    @FXML
+    private TextField NTN;
+
+    @FXML
+    private TextField cBalance;
+
+    @FXML
+    private DatePicker asOf;
+
+    @FXML
+    private ChoiceBox<String> pGroup;
+
+    @FXML
+    private TextField email;
+
+    @FXML
+    private Button save;
+
+    @FXML
+    private Button saveAndNew;
+    
+    Connection con;
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        pGroupList.add("General");
+        pGroup.getItems().addAll(pGroupList);
+        
+        con = MyConnection.getConnection();
     }    
-    
+    @FXML
+    void handleAction(ActionEvent event) throws SQLException {
+        if(event.getSource()== save){
+            String query = "INSERT INTO `vendors`(`Name`, `NTN`, `phone`, `Address`, `ShippingAddress`, `cBalance`, `pGroup`, `Email`, `AsOf`)  "
+                    + "VALUES ('"+Cname.getText()+"','"+NTN.getText()+"','"+pNum.getText()+"','"+addr.getText()+"','"+sAddr.getText()+"',"
+                    + "'"+cBalance.getText()+"','"+pGroup.getValue() + "','"+email.getText()+"','"+asOf.getValue()+"')";
+            Statement st = con.createStatement();
+            st.executeUpdate(query);
+            con.close();
+        }
+    }
+
 }
--- src/marsons/yard/CustomersController.java
+++ src/marsons/yard/CustomersController.java
@@ -37,7 +37,7 @@ public class CustomersController implements Initializable {
             Parent root1 = (Parent) fxmlLoader.load();
             Stage stage = new Stage();
             stage.setScene(new Scene(root1));  
-           
+       
             stage.show(); 
         }
     }
