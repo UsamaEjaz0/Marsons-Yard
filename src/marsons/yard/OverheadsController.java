1c60893441e9abb046828adbd902a74602641f23
UsamaEjaz0 <uejaz0@gmail.com>
2020-07-17 16:46:43 +0500

    Added Overheads, direct and indirect expenses

--- src/marsons/yard/OverheadsController.java
+++ src/marsons/yard/OverheadsController.java
@@ -5,12 +5,17 @@
  */
 package marsons.yard;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -22,12 +27,23 @@ public class OverheadsController implements Initializable {
     /**
      * Initializes the controller class.
      */
+    
+    @FXML
+    private Button addCat;
+    
     @FXML
     private Button addExpense;
 
     @FXML
-    void handleAction(ActionEvent event) {
-        
+    void handleAction(ActionEvent event) throws IOException {
+        if (event.getSource() == addExpense){
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("AddOverheads.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));  
+            stage.setMaximized(true);
+            stage.show();
+        }
     }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
